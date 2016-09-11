0186ff49fa97a630c4ee37ee6ff377a1ef08f1c2
Ross Paine <ashtermet@gmail.com>
2016-09-11 22:20:01 +0800

    first draft of a scraper Cockburn - not a nice website

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,62 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
+require 'uri'
+require 'logger'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+@logger = Logger.new(STDOUT)
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+starting_url = URI('http://www.cockburn.wa.gov.au')
+starting_url.path = '/News/Community_Consultation/'
+
+comment_url = URI('http://www.cockburn.wa.gov.au/OnlineSubmissions')
+
+class String
+  def is_da?
+    !self.match(/development application/).nil?
+  end
+end
+
+@agent = Mechanize.new
+
+doc = @agent.get(starting_url.to_s)
+selector = 'div#Content > ol > li'
+# http://www.cockburn.wa.gov.au/templates/template48/summary.asp?TemplateID=48&EventID=4142
+
+doc.search(selector).each do |e|
+  if e.inner_html.is_da?
+    link = e.search('a')
+    if link.attr('href').to_s =~ /Javascript:sum\('(\d+)'\)/
+      record = {}
+
+      record['council_reference'] = $1
+      @logger.info("Using #{record['council_reference']} as council_reference")
+
+      starting_url.path = '/templates/template48/summary.asp'
+      starting_url.query = "TemplateID=48&EventID=#{record['council_reference']}"
+
+      record['info_url'] = starting_url.to_s
+
+      @logger.debug("Fetching #{record['info_url']}")
+
+      da_doc = @agent.get(record['info_url'])
+
+      record['address'] = da_doc.xpath("//th[text()='Name: ']/following-sibling::th").inner_html.strip + ", WA"
+      @logger.info("Using #{record['address']} as address")
+
+      description_html = da_doc.xpath("//table/tr/th/p[contains(., 'Description:')]/parent::th/following-sibling::td").inner_html
+      description_text = description_html.gsub(/<br>/, "")
+      record['description'] = description_text.match(/^(.*)The plans can be viewed/m)[1]
+
+      record['date_scraped']  = Date.today.to_s
+      if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+        ScraperWiki.save_sqlite(['council_reference'], record)
+      else
+        puts "Skipping already saved record " + record['council_reference']
+      end
+    else
+      @logger.info("#{link.attr('href')} doesn't look like a link to a DA page")
+    end
+  else
+    @logger.info("Skipping #{e.search('a').inner_html} because it don't look like a DA")
+  end
+end
